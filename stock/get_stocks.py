0d48e89406ebd948d299bbd8b0bed7bc121a5938
Daniel Kiss <littlesnorrboy@gmail.com>
2017-10-04 15:39:12 +0200

    Add deltas to the stocks

--- stock/get_stocks.py
+++ stock/get_stocks.py
@@ -4,10 +4,30 @@ from datetime import datetime
 import requests
 
 
+def get_delta(index, stocks, n):
+    try:
+        return (int(stocks[index]['price']) - int(stocks[index - n]['price']))
+    except (KeyError, IndexError):
+        return 0
+
+
+def calculate_deltas(stocks):
+    for i, stock in enumerate(stocks):
+        stock['delta_1'] = get_delta(i, stocks, 1)
+        stock['delta_2'] = get_delta(i, stocks, 2)
+        stock['delta_3'] = get_delta(i, stocks, 3)
+        stock['delta_4'] = get_delta(i, stocks, 4)
+        stock['delta_5'] = get_delta(i, stocks, 5)
+        stock['delta_6'] = get_delta(i, stocks, 6)
+        stock['delta_7'] = get_delta(i, stocks, 7)
+    return stocks
+
+
 def main():
     result = requests.get('https://www.coinbase.com/api/v2/prices/BTC-HUF/historic?period=year')
     with open('BTC-HUF.csv', 'w') as f:
         prices = result.json()['data']['prices']
+        prices = calculate_deltas(prices)
         writer = csv.DictWriter(f, prices[0].keys())
         writer.writeheader()
         for row in prices:
